5464acb8277376e494c04ecc8b8adc13e33e5637
zaklance <wosewick@gmail.com>
2024-04-11 08:57:40 -0400

    done

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function introduction(name) {
     console.log(`Hi, my name is ${name}.`);
 }
 function introductionWithLanguage(name, language) {
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.')
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.')
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
